26a189a7af115ef018d797e1b1a2a2a226ba927e
Atharv1191 <119674561+Atharv1191@users.noreply.github.com>
2023-07-06 15:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
+ 
 const person = {
   name: "John Doe",
   age: 80,
 };
-s is exactly same as mentioned in the problem statement
-var person={
-	name: "Jhon Doe",
-	age: 80;
-	
-};
-
-var team = players
-var team1 = [...players]
-var cap1 = {...person}
+ 
+// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+var team= players;
+var team1 =players.slice();
+var cap1 = Object.assign({}, person);
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
 
 
 
